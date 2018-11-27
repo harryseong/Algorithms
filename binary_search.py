1b215c7181bba4060ee2e788954e1ee331a34be0
Harry Seong <harry@northwestern.edu>
2018-11-27 15:58:53 -0600

    Recursive binary search added.

--- binary_search.py
+++ binary_search.py
@@ -15,3 +15,22 @@ def binary_search(array, number):
 
     print('# of Steps: ' + str(counter))
     return None
+
+
+# Recursive binary search
+def binary_search_2(arr, find_this):
+    # Edge case #1: Ensure that array length is greater than 0.
+    if len(arr) == 0:
+        print('Could not find number')
+        return False
+    if len(arr) % 2 == 1:
+        midpoint = (len(arr) + 1) // 2
+    else:
+        midpoint = len(arr) // 2
+    if find_this == arr[midpoint]:
+        print('Found ' + str(find_this) + ' in the array!')
+        return True
+    elif find_this < arr[midpoint]:
+        binary_search(arr[0: midpoint], find_this)
+    else:
+        binary_search(arr[midpoint + 1: len(arr)], find_this)
